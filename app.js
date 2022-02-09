78a86735af0d2fca0af14463d474dc70c95cc452
Martin Loman <martin.loman@gmail.com>
2022-02-09 11:10:50 +0100

    Changed var to let

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 // Här hämtar jag referenser till objekt/element i DOMen
-var btnObject = document.getElementById("btnObject")
-var btnHTML = document.getElementById("btnHTML")
-var outputContainer = document.getElementById("outputContainer")
-var txtMessage = document.getElementById("txtMessage")
+let btnObject = document.getElementById("btnObject")
+let btnHTML = document.getElementById("btnHTML")
+let outputContainer = document.getElementById("outputContainer")
+let txtMessage = document.getElementById("txtMessage")
 
 // Jag knyter en eventlyssnare till klick-eventet på ena knappen.
 btnObject.onclick = function (e) {
@@ -18,12 +18,12 @@ btnObject.onclick = function (e) {
     som ska ha eventlyssnare kopplat till sig.
   */
 
-  var inputText = txtMessage.value
+  let inputText = txtMessage.value // hämtar värdet i textboxen txtMessage och lägger i variabeln inputText
 
-  var newTextDiv = document.createElement("div")
+  let newTextDiv = document.createElement("div") // Skapar ett nytt element/nod som är en div
   newTextDiv.innerText = inputText
 
-  outputContainer.append(newTextDiv)
+  outputContainer.append(newTextDiv) // lägger in div:en i DOMen. Det är först nu den syns!
 }
 
 // Till den andra knappen knyter jag en eventlyssnare på ett annat
@@ -39,8 +39,8 @@ btnHTML.addEventListener("click", function(e) {
   */
 
 
-  var inputText = txtMessage.value
-  var html = `<div>${inputText}</div>` // Det här är en så kallad "template string". Den fungerar som en f-string i Python.
+  let inputText = txtMessage.value
+  let html = `<div>${inputText}</div>` // Det här är en så kallad "template string". Den fungerar som en f-string i Python. Det är "bakåtlutande fnuttar".
 
   //https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
   outputContainer.insertAdjacentHTML("beforeend", html)
